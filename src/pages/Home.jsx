8e65d589f9b0675aa7cb4e39d3ae18d899228aa5
Diego-monfort-landot <diegomonfort1989@gmail.com>
2023-10-12 21:02:43 +0200

    img and typewriter aded- serching for new ideas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import Typewriter from 'typewriter-effect'
-import bild from '../images/curbi.jpg'
 import './Home.css'
 
 export const Home = () => {
   return (
     <>
       <div className='mainsection'>
-        <div className='imaagesection'>
-          <img src={bild} />
-        </div>
-        <div className='überschrift'>
+        <div className='imagesection'>
+          <div className='überschrift'>
 
-          <h2 className='under'><Typewriter
-            options={{
-              strings: ['Welcome on my Portfolio', '{ I m DiegoDev }'],
-              autoStart: true,
-              loop: true,
-            }}
-          /></h2>
+            <h2 className='under'><Typewriter
+              options={{
+                strings: ['Welcome on my Portfolio', '{ I m DiegoDev }'],
+                autoStart: true,
+                loop: true,
+              }}
+            /></h2>
+
+
+          </div>
+
         </div>
       </div>
 
@@ -28,14 +29,16 @@ export const Home = () => {
 
         </div>
         <div className='texto'>
+
+
+
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque laborum eius, excepturi nemo dolor, inventore aspernatur unde iste quaerat adipisci veritatis ullam reiciendis nihil quam quibusdam odit, distinctio neque esse architecto recusandae quae dolorem? Vero dolor error id nisi reprehenderit, illum minima! Similique odio iure assumenda doloremque suscipit neque itaque? Nemo rem blanditiis veritatis, laboriosam numquam vitae officiis maiores, officia, repellendus nesciunt soluta ducimus est eaque. Qui numquam quidem nisi, ratione aut hic a? Cupiditate quod doloribus modi nobis architecto magnam hic. Quis dolorem fugiat dolor adipisci, neque, accusamus architecto iusto odio rerum ratione molestiae nobis perferendis! Quia, placea
 
 
-            <h2>huhu</h2>
+            <br />
 
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci laboriosam tempore neque, repellat commodi ut. Molestias delectus exercitationem, quod commodi optio cum. Necessitatibus natus fuga ipsum repudiandae ratione obcaecati maxime consequatur accusamus! Temporibus cum blanditiis magnam quidem, voluptatum vero aspernatur minus qui ex pariatur et provident porro consectetur quos atque iste soluta laudantium sapiente. Officiis dolores minus beatae provident dignissimos quasi iure illum, nostrum accusantium asperiores et quia excepturi totam molestiae sapiente magni quae similique voluptates aperiam officia consequatur, quibusdam quisquam? Dicta, corrupti rem iure modi quam cum odio, nihil alias commodi itaque repudiandae quaerat maiores ducimus. Impedit quo accusamus, non ut nulla debitis id nisi tempore adipisci expedita cum aperiam quis, mollitia exercitationem. Consectetur, rerum doloremque. Exercitationem harum quae, officia, fuga adipisci cupiditate excepturi eligendi totam sapiente sunt at cum vero magni obcaecati facere quos ex necessitatibus? Fuga aliquam iusto adipisci molestias sunt, magnam ipsam ad eum, ex veritatis quidem officiis repellendus voluptate? Dolores expedita, laudantium dolore necessitatibus ducimus natus iusto est, repudiandae molestiae nulla debitis voluptates nobis quidem iste. Numquam quod ea ex? Omnis reiciendis quae, autem dolorem nam tenetur delectus aliquam nesciunt impedit voluptatibus. Et tenetur tempora ea, cupiditate, odio fugiat dolorem necessitatibus quia expedita explicabo provident?
-            <h2>huhu</h2>
-
+            <br />
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci laboriosam tempore neque, repellat commodi ut. Molestias delectus exercitationem, quod commodi optio cum. Necessitatibus natus fuga ipsum repudiandae ratione obcaecati maxime consequatur accusamus! Temporibus cum blanditiis magnam quidem, voluptatum vero aspernatur minus qui ex pariatur et provident porro consectetur quos atque iste soluta laudantium sapiente. Officiis dolores minus beatae provident dignissimos quasi iure illum, nostrum accusantium asperiores et quia excepturi totam molestiae sapiente magni quae similique voluptates aperiam officia consequatur, quibusdam quisquam? Dicta, corrupti rem iure modi quam cum odio, nihil alias commodi itaque repudiandae quaerat maiores ducimus. Impedit quo accusamus, non ut nulla debitis id nisi tempore adipisci expedita cum aperiam quis, mollitia exercitationem. Consectetur, rerum doloremque. Exercitationem harum quae, officia, fuga adipisci cupiditate excepturi eligendi totam sapiente sunt at cum vero magni obcaecati facere quos ex necessitatibus? Fuga aliquam iusto adipisci molestias sunt, magnam ipsam ad eum, ex veritatis quidem officiis repellendus voluptate? Dolores expedita, laudantium dolore necessitatibus ducimus natus iusto est, repudiandae molestiae nulla debitis voluptates nobis quidem iste. Numquam quod ea ex? Omnis reiciendis quae, autem dolorem nam tenetur delectus aliquam nesciunt impedit voluptatibus. Et tenetur tempora ea, cupiditate, odio fugiat dolorem necessitatibus quia expedita explicabo provident?
           </p>
 
