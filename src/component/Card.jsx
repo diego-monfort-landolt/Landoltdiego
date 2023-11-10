20889bd6636bea825aeaf7cc93c61ec22defb29f
Diego-monfort-landot <diegomonfort1989@gmail.com>
2023-11-10 20:24:37 +0100

    delet spaces

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -7,16 +7,12 @@ import Diego from '../images/diego-landolt.png'
 const Card = () => {
 
   const [isOpen, setIsOpen] = useState(false)
- 
-
   const heart = () => {
     setLike(innerHTML.style.color = 'red')
   }
-  
   return (
     <>
       <div className='card-container'>
-
         <motion.div transition={{ layout: { duration: 1, type: 'spring' } }} layout onClick={() => setIsOpen(!isOpen)} className='card'>
           <motion.h2 layout='position'>👆 Diego Landolt 👆</motion.h2>
           {isOpen && (
@@ -25,7 +21,6 @@ const Card = () => {
                 <div className='text-cont'>
                   <p>Bienvenidos a mi nuevo portfolio, creado con React vite. espero que os guste. actualmente sigo aprendiendo react y javascript ya que quiero trabajar en este sector. echar un vistazo a mi web y si quereis contactar conmigo podeis hacerlo via linkedin o github.</p>
                   <p><b>Espero que te guste</b> </p>
-
                 </div>
                 <div className="img-cont">
                   <img className='card-diego-img' src={Diego} alt='a photo of me' />
@@ -34,10 +29,7 @@ const Card = () => {
             </motion.div>
           )}
         </motion.div>
-
       </div>
-
-
     </>
   )
 }
