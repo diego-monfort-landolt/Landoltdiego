121ea84c65cbad460a9a9bf65f4f6b3ef5a9e97f
Diego-monfort-landot <diegomonfort1989@gmail.com>
2023-12-18 13:22:07 +0100

    added save in the localstorage -if

--- src/component/PopUp.jsx
+++ src/component/PopUp.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './popup.css'
 
 const Popup = () => {
-  const [showPopup, setShowPopup] = useState(true);
+  const [showPopup, setShowPopup] = useState(false);
 
-  const handleClose = () => {
-    setShowPopup(false);
-    
-    
-  };
+  useEffect(() => {
+    if (!localStorage.getItem('first-time')) {
+      setShowPopup(true);
+      localStorage.setItem('first-time', 1)
+    }
+  }, [])
 
   return (
     <>
@@ -17,7 +18,7 @@ const Popup = () => {
           <div className="popup-content"> 
             <h2>Bienvenido a mi sitio web</h2>
             <p>¡Gracias por visitar mi sitio web!</p>
-            <button className="close" onClick={handleClose}>❌</button>
+            <button className="close" onClick={() => {setShowPopup(false)}}>❌</button>
           </div>
         </div>
         
