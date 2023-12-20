f40ca10970abfcde7b780c6e56b47e6f0b3c419f
Diego-monfort-landot <diegomonfort1989@gmail.com>
2023-12-20 19:35:08 +0100

    added typewriter on the second title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,7 +41,13 @@ export const Home = () => {
 
       <main className='main'>
         <div className='main-container'>
-          <h2   className={`end-header appear-animation ${isVisible ? 'visible' : ''}`}>Bienvenido 👋 </h2>
+          <h2   className={`end-header appear-animation ${isVisible ? 'visible' : ''}`}>   <h2 className='under'><Typewriter
+              options={{
+                strings: ['Bienvenido 👋 ', '{ Welcome }', 'Herzlich Wilkommen' ],
+                autoStart: true,
+                loop: true,
+              }}
+            /></h2> </h2>
           <Card />
         </div>
         <div className='text-section'>
