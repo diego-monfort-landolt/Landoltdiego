54810419dd7c492baaaac6b9b305c429fd79210a
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-01-27 21:30:11 +0100

    a

--- src/component/experience-cards/Info.jsx
+++ src/component/experience-cards/Info.jsx
@@ -1,40 +1,40 @@
 import './review.css'
 import { AiOutlineClose  } from "react-icons/ai";
 
-const info = [
-  {
-    id: 1,
-    info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
-  },
-  {
-    id: 2,
-    info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
-  },
-  {
-    id: 3,
-    info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
-  },
-  {
-    id: 4,
-    info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
-  }, 
-  {
-    id: 5,
-    info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
-  },
-  {
-    id: 6,
-    info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
-  },
-]
+// const info = [
+//   {
+//     id: 1,
+//     info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
+//   },
+//   {
+//     id: 2,
+//     info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
+//   },
+//   {
+//     id: 3,
+//     info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
+//   },
+//   {
+//     id: 4,
+//     info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
+//   }, 
+//   {
+//     id: 5,
+//     info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
+//   },
+//   {
+//     id: 6,
+//     info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
+//   },
+// ]
 
-const Info = ({info}) => {
+const Info = () => {
   return (
     <>
     <div className='overlay'>
       <div className='cont-model'>
         <div className='model-head'>
-          <h3 className='info-txt'>{info}Popup</h3>
+          <h3 className='info-txt'>Popup</h3>
         </div>
          <button className='close-btn'><AiOutlineClose size={20} /></button> 
       </div>
