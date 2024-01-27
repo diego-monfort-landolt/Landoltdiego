a328f09fcc2dbed2e1024a8ea10c7b0225deafc6
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-01-28 00:14:22 +0100

    added styles and logic

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
+
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import './Navbar.css'
 
 
@@ -26,8 +28,23 @@ export const Navbar = () => {
       </nav>
 
       <h2 className='welcometxt'> A warm Welcom!</h2>
-      <p className='txtnavbar'>this is my protfolio that i am creating with react, there are still some bugs that i want to fix.
-        everything step by step. </p>
+      <div className='textsection'>
+        <p className='txtnavbar'>This is my protfolio that i am creating with react, there are still some bugs that i want to fix.
+          everything step by step. </p>
+        <div className='iconsection'>
+          <a href='https://github.com/diego-monfort-landolt'>
+            <AiFillGithub color='red' size='40px' />
+          </a>
+          <a href='https://www.linkedin.com/in/diego-monfort-landolt/'>
+            <AiFillLinkedin size='40px' color='red' />
+          </a>
+        </div>
+        <div className='link-home'>
+          <NavLink to='/'>Home</NavLink>
+        </div>
+
+      </div>
+
     </>
   )
 }
