8c7b893edc54b4ec1325b3e5a1f6578f9735d1a9
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-01-30 22:16:03 +0100

    A

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Navbar to='/'/> 
      <Routes > 
-        <Route path='/' element={<Home />} />
+        <Route exact path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
