4460c17a899d23e12a09e9a490c82301b8d1c2b2
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-02-01 19:53:24 +0100

    a

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -16,6 +16,7 @@ export const Navbar = () => {
           <span  ></span>
           <span  ></span>
         </div>
+        
         <ul className={menuOpen ? "open" : ""}>
           <li> <NavLink to='/'>Home</NavLink> </li>
           <li> <NavLink to='/about'>About</NavLink> </li>
