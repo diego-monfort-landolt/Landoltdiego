001553d46ebf1da24aa9bdf4e1670421a2e5d53e
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-02-04 21:09:00 +0100

    change spaces. todoo ,emu is not working weel

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
