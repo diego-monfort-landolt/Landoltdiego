21331de104e4abc4898b31e9570b449715c85dbe
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-02-04 22:58:50 +0100

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,44 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Navbar } from './component/Navbar'
 import { About, Contact, Home } from './pages/'
+import Typewriter from 'typewriter-effect'
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 function App() {
   return (
     <>
-    <div>
-      <Navbar />
-    </div>
-      <Routes >
-        <Route exact path='/' element={<Home />} />
+      <div>  
+        <Navbar />
+      </div>
+      <Routes handler={Home} path="/" > 
+        <Route  exact path="/"  element={<Home />} />
         <Route path='about' element={<About />} />
         <Route path='contact' element={<Contact />} />
       </Routes>
+
+      <h2 className='First-Titel'><Typewriter
+              options={{
+                strings: ['Bienvenido 👋 ', 'Welcome to my Portfolio', 'Herzlich Wilkommen', '{ I m DiegoDev }'],
+                autoStart: true,
+                loop: true,
+              }}
+            /></h2>
+            <p className='text-app'>
+            Herzlich Willkommen! Wir freuen uns sehr, Sie hier zu begrüßen. Obwohl wir eine kleine Gemeinschaft sind, haben wir ein großes Herz und freuen uns darauf, Sie kennenzulernen. Genießen Sie Ihren Aufenthalt!
+
+            </p>
+
+
+            <div className='icons-app'>
+
+               <a href='https://github.com/diego-monfort-landolt'>
+              <AiFillGithub color='red' size='40px' />
+            </a>
+            <a href='https://www.linkedin.com/in/diego-monfort-landolt/'>
+              <AiFillLinkedin size='40px' color='red' />
+            </a>
+            </div>
+              
     </>
   )
 }
