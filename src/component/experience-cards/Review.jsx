47ccd0a53a052dfcf229ddc6fe4b2091f24d321d
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-02-06 20:02:49 +0100

    a

--- src/component/experience-cards/Review.jsx
+++ src/component/experience-cards/Review.jsx
@@ -3,6 +3,33 @@ import Info from './Info'
 import './review.css'
 import PropTypes  from 'prop-types'
 
+const info = [
+  {
+    id: 1,
+    info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
+  },
+  {
+    id: 2,
+    info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
+  },
+  {
+    id: 3,
+    info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
+  },
+  {
+    id: 4,
+    info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
+  }, 
+  {
+    id: 5,
+    info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
+  },
+  {
+    id: 6,
+    info: 'Me enamoro de react, un lenguage tan precioso que me facina cada dia mas y mas'
+  },
+]
+
 const Review = ({ title, text, image }) => {
   return (
     <>
@@ -15,7 +42,6 @@ const Review = ({ title, text, image }) => {
       <div className='container-btn'>
         <button className='review-btn' onClick={() => {alert('will be activated in the next update')}}>Más info</button>
         {/* text area from info - when click this */}
-        
         <Info />
       </div>
     </div>
