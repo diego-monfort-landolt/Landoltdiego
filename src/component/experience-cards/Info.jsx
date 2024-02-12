e557d3508f3de9d7120850b2eaa738ec1441e879
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-02-12 20:26:35 +0100

    a

--- src/component/experience-cards/Info.jsx
+++ src/component/experience-cards/Info.jsx
@@ -2,7 +2,6 @@ import './review.css'
 import { AiOutlineClose } from "react-icons/ai";
 import PropTypes  from 'prop-types'
 
-
 const Info = ({ children, estado, cambiarEstado }) => {
   return (
     <>
@@ -15,7 +14,7 @@ const Info = ({ children, estado, cambiarEstado }) => {
             <button className='close-btn' onClick={() => cambiarEstado(false)} ><AiOutlineClose size={20} /></button>
 
             {children}
-            
+
           </div>
         </div>
       }
--- src/component/experience-cards/Reviews.jsx
+++ src/component/experience-cards/Reviews.jsx
@@ -64,5 +64,4 @@ const Reviews = () => {
     </>
   )
 }
-
 export default Reviews
