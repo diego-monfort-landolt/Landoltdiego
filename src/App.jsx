a40fc94da14aefc1e6b3719230e0d4bf2b8cc824
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-02-18 21:10:45 +0100

    removed thanks page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Navbar } from './component/Navbar'
 import { About, Contact, Home } from './pages/'
-import Thanks from './component/form/Thanks'
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
-        <Route path='/thanks' element={<Thanks />} />
         <Route path='*' element={<Home />} />  
       </Routes>
     </>
