c5aed4e4044ea324a0400b2a995cd5b921373f10
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-04-10 14:01:27 +0200

    added on the home the export - and change the rutes

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path='About' element={<About />} />
         <Route path='Contact' element={<Contact />} />
-        <Route path='*' element={<Home />} />  
+        <Route path='/*' element={<Home />} />  
       </Routes>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -81,3 +81,4 @@ export const Home = () => {
     </>
   )
 }
+export default Home;
