7900445f546b37f5313220da97644d91579702d2
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-05-12 19:06:03 +0200

    added new images src - route from google fotos

--- src/component/ContactCards/ContactCard.jsx
+++ src/component/ContactCards/ContactCard.jsx
@@ -1,23 +1,18 @@
 
 import './ContactCard.css'
-import im1 from './imgs/im1.jpg'
-import im2 from './imgs/im2.jpg'
-import im3 from './imgs/im3.jpg'
-import im4 from './imgs/im4.jpg'
-import im5 from './imgs/im5.jpg'
-import im6 from './imgs/im6.jpg'
 
 const ContactCard = () => {
   return (
     <>
     <div className='ContactCon'>
       <div className='contact-img'>
-        <img className='conimg' src={im1} alt='' />
-        <img className='conimg' src={im2} alt='' />
-        <img className='conimg' src={im3} alt='' />
-        <img className='conimg' src={im4} alt='' />
-        <img className='conimg' src={im5} alt='' />
-        <img className='conimg' src={im6} alt='' />
+        <img className='conimg' src={'https://scontent-mad2-1.xx.fbcdn.net/v/t39.30808-6/398777492_1017085382907800_2347621024280635812_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=UA60YmxYXnYQ7kNvgE8KQxG&_nc_ht=scontent-mad2-1.xx&oh=00_AYD7OytcCMDl842u2YPcNRD9PheHg4XPrbE_padvnNEFUw&oe=6646CFFB'} alt='' />
+        <img className='conimg' src={'https://scontent-mad1-1.xx.fbcdn.net/v/t39.30808-6/321300415_1345406439551102_908557903613551749_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=vNogs3JQSu4Q7kNvgGPRPc0&_nc_ht=scontent-mad1-1.xx&oh=00_AYBjSOrwfysj0Za18zmDvxHEgGJLl1QwGDz_sflu7ngWgA&oe=6646CC5E'} alt='' />
+        <img className='conimg' src={'https://scontent-mad2-1.xx.fbcdn.net/v/t39.30808-6/295010499_737415470874794_5116690293415564987_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=v5nfiRqQB8IQ7kNvgHY4djC&_nc_ht=scontent-mad2-1.xx&oh=00_AYDXw36RlTaUD7gbY31gw0Ix7896ZTrAWrftHP32d0ps3w&oe=6646DA95'} alt='' />
+        <img className='conimg' src={'https://scontent-mad1-1.xx.fbcdn.net/v/t1.6435-9/203124602_496135335002810_3902919108576945344_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=lmail_425lcQ7kNvgF8O9j4&_nc_ht=scontent-mad1-1.xx&oh=00_AYApFSgAcb4v-U-Q_LTKqHbcM7SdB-8DsFjVNhFvbU8pFg&oe=666852B0'} alt='' />
+        <img className='conimg' src={'https://scontent-mad2-1.xx.fbcdn.net/v/t1.6435-9/64756026_105219707427710_4937933533822844928_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=eLYO3bdjkowQ7kNvgFrBfm9&_nc_ht=scontent-mad2-1.xx&oh=00_AYC9y8J2MVzsUd5KTDquo7CqLR70yL0p_J6fIqOI6FkbBg&oe=66685BF0'} alt='' />
+        <img className='conimg' src={'https://scontent.fvlc4-1.fna.fbcdn.net/v/t1.6435-9/64706118_104988194117528_1525426157082640384_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=LQvOQjONO1gQ7kNvgGLvh5h&_nc_ht=scontent.fvlc4-1.fna&oh=00_AYAthJPS_GfKrU0768ExKIu2pOXnq1_iC9q54zPN3jXQJg&oe=66687393'} alt='' />
+        <img className='conimg' src={'https://scontent.fvlc4-1.fna.fbcdn.net/v/t39.30808-6/240066820_533748114574865_1238857492568033965_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=7WP5Yx2lt5MQ7kNvgGDIuaq&_nc_ht=scontent.fvlc4-1.fna&oh=00_AYD8SBEZ5EfQZ7gAXll-yPiMQ796d21NnncvulBGMqCuvA&oe=6646BF55'} alt='' />
       </div>
     </div>
     </>
