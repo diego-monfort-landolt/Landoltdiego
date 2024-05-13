76270f0674ddc2762695e9113a4614196f13cb6a
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-05-13 20:20:40 +0200

    added foto from linked in whit open to work

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { motion } from 'framer-motion'
 import '../component/Card.css'
-import Diego from '../images/diego-landolt.png'
 
 
 const Card = () => {
@@ -18,7 +17,7 @@ const Card = () => {
                 <div className='text-cont'>
                   <p>Bienvenidos a mi portfolio, creado con React vite. Actualmente sigo aprendiendo react, TypeScript y node.js ya que quiero trabajar en este sector. Echar un vistazo a mis projectos y si quereis contactar conmigo podeis hacerlo por el formulario de contacto o los iconos de Linkedin o Github.</p>
                   <div className="img-cont">
-                  <img className='card-diego-img' src={Diego} alt='a photo of me' />
+                  <img className='card-diego-img' src={'https://media.licdn.com/dms/image/D4D35AQGU7rvZdrQb9g/profile-framedphoto-shrink_200_200/0/1711478836290?e=1716231600&v=beta&t=j78ql08PVo_YDe10auijEE2aHWsEiU82QtOS7SI3SIk'} alt='a photo of me' />
                 </div>
                 </div> 
               </div>
