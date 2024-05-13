5360c61842d8defceb7fa45c03df7644ef4e2413
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-05-13 20:49:11 +0200

    d

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -13,7 +13,7 @@ const Card = () => {
             <motion.div>
               <div className="cont">
                 <div className='text-cont'>
-                  <p>Bienvenidos a mi portfolio, creado con React vite. Actualmente sigo aprendiendo react, TypeScript y node.js ya que quiero trabajar en este sector. Echar un vistazo a mis projectos y si quereis contactar conmigo podeis hacerlo por el formulario de contacto o los iconos de Linkedin o Github.</p>
+                  <p>Bienvenidos a mi portfolio, creado con React vite. Echar un vistazo a mis projectos y si quereis contactar conmigo podeis hacerlo por el formulario de contacto o los iconos de Linkedin o Github.</p>
                   <div className="img-cont">
                   <img className='card-diego-img' src={'https://media.licdn.com/dms/image/D4D35AQGU7rvZdrQb9g/profile-framedphoto-shrink_200_200/0/1711478836290?e=1716231600&v=beta&t=j78ql08PVo_YDe10auijEE2aHWsEiU82QtOS7SI3SIk'} alt='a photo of me' />
                 </div>
