50cdd63ec9bc002912da114ed55d91c83792c0d6
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-05-13 20:47:53 +0200

    a

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { motion } from 'framer-motion'
 import '../component/Card.css'
 
-
 const Card = () => {
   const [isOpen, setIsOpen] = useState(false)
   return (
