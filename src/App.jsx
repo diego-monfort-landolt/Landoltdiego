56ffee576bbbc68bd0b9eea746a1ce2cc251f748
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-06-27 21:51:57 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 function App() {
-  
   const location = useLocation();
   useEffect(() => {
     let currentPath = location.pathname;
