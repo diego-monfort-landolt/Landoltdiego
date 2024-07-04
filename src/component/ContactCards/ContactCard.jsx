2e0ab715de4b47c91afacb7bfc7667f37af94a79
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-07-04 22:14:48 +0200

    s

--- src/component/ContactCards/ContactCard.jsx
+++ src/component/ContactCards/ContactCard.jsx
@@ -5,14 +5,14 @@ const ContactCard = () => {
     <>
     <div className='ContactCon'>
       <div className='contact-img'>
-        <img className='conimg' src={'https://scontent-mad2-1.xx.fbcdn.net/v/t39.30808-6/398777492_1017085382907800_2347621024280635812_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=UA60YmxYXnYQ7kNvgE8KQxG&_nc_ht=scontent-mad2-1.xx&oh=00_AYD7OytcCMDl842u2YPcNRD9PheHg4XPrbE_padvnNEFUw&oe=6646CFFB'} alt='' />
-        <img className='conimg' src={'https://scontent-mad1-1.xx.fbcdn.net/v/t39.30808-6/321300415_1345406439551102_908557903613551749_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=vNogs3JQSu4Q7kNvgGPRPc0&_nc_ht=scontent-mad1-1.xx&oh=00_AYBjSOrwfysj0Za18zmDvxHEgGJLl1QwGDz_sflu7ngWgA&oe=6646CC5E'} alt='' />
-        <img className='conimg' src={'https://scontent-mad2-1.xx.fbcdn.net/v/t39.30808-6/295010499_737415470874794_5116690293415564987_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=v5nfiRqQB8IQ7kNvgHY4djC&_nc_ht=scontent-mad2-1.xx&oh=00_AYDXw36RlTaUD7gbY31gw0Ix7896ZTrAWrftHP32d0ps3w&oe=6646DA95'} alt='' />
-        <img className='conimg' src={'https://scontent-mad1-1.xx.fbcdn.net/v/t1.6435-9/203124602_496135335002810_3902919108576945344_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=lmail_425lcQ7kNvgF8O9j4&_nc_ht=scontent-mad1-1.xx&oh=00_AYApFSgAcb4v-U-Q_LTKqHbcM7SdB-8DsFjVNhFvbU8pFg&oe=666852B0'} alt='' />
-        <img className='conimg' src={'https://scontent-mad1-1.xx.fbcdn.net/v/t1.6435-9/203124602_496135335002810_3902919108576945344_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=lmail_425lcQ7kNvgF8O9j4&_nc_ht=scontent-mad1-1.xx&oh=00_AYApFSgAcb4v-U-Q_LTKqHbcM7SdB-8DsFjVNhFvbU8pFg&oe=666852B0'} alt='' />
-        <img className='conimg' src={'https://scontent-mad2-1.xx.fbcdn.net/v/t1.6435-9/64756026_105219707427710_4937933533822844928_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=eLYO3bdjkowQ7kNvgFrBfm9&_nc_ht=scontent-mad2-1.xx&oh=00_AYC9y8J2MVzsUd5KTDquo7CqLR70yL0p_J6fIqOI6FkbBg&oe=66685BF0'} alt='' />
-        <img className='conimg' src={'https://scontent.fvlc4-1.fna.fbcdn.net/v/t1.6435-9/64706118_104988194117528_1525426157082640384_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=LQvOQjONO1gQ7kNvgGLvh5h&_nc_ht=scontent.fvlc4-1.fna&oh=00_AYAthJPS_GfKrU0768ExKIu2pOXnq1_iC9q54zPN3jXQJg&oe=66687393'} alt='' />
-        <img className='conimg' src={'https://scontent.fvlc4-1.fna.fbcdn.net/v/t39.30808-6/240066820_533748114574865_1238857492568033965_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=7WP5Yx2lt5MQ7kNvgGDIuaq&_nc_ht=scontent.fvlc4-1.fna&oh=00_AYD8SBEZ5EfQZ7gAXll-yPiMQ796d21NnncvulBGMqCuvA&oe=6646BF55'} alt='' />
+        <img className='conimg' src={'https://cdn.pixabay.com/photo/2015/09/23/03/55/sunset-952876_1280.jpg'} alt='sunset' />
+        <img className='conimg' src={'https://cdn.pixabay.com/photo/2016/10/29/20/39/gelsenkirchen-1781473_1280.jpg'} alt='' />
+        <img className='conimg' src={'https://cdn.pixabay.com/photo/2020/01/12/18/44/sunset-4760808_1280.jpg'} alt='' />
+        <img className='conimg' src={'https://cdn.pixabay.com/photo/2020/05/07/08/50/italy-5140706_1280.jpg'} alt='' />
+        <img className='conimg' src={'https://cdn.pixabay.com/photo/2017/09/08/20/32/sunset-2730045_1280.jpg'} alt='' />
+        <img className='conimg' src={'https://cdn.pixabay.com/photo/2017/09/26/13/37/nature-2788640_1280.jpg'} alt='' />
+        <img className='conimg' src={'https://cdn.pixabay.com/photo/2020/02/06/15/33/sunset-4824669_1280.jpg'} alt='' />
+        <img className='conimg' src={'https://cdn.pixabay.com/photo/2017/09/08/20/32/sunset-2730045_1280.jpg'} alt='' />
       </div>
     </div>
     </>
