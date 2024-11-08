365515385b39aaffba1da7e2fa2c9df669e83431
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-11-08 13:53:25 +0100

    rm blanc space

--- src/component/clock/Time.jsx
+++ src/component/clock/Time.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './time.css'
 
 const Time = () => {
+  
   let time = new Date().toLocaleTimeString()
     const [currentTime, setCurrentTime] =  useState(time)
     const updateTime = () => {
