2a1f8f39bca5771411f2518fd83851885dacd04d
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-12-05 15:52:08 +0100

    rm blanc space

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { motion } from 'framer-motion'
 import '../component/Card.css'
-
 const Card = () => {
   const [isOpen, setIsOpen] = useState(false)
   return (
