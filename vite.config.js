cac5a922793bac659d84790c19e4e58bd614064c
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-12-25 00:38:30 +0100

    f

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base:'https://diego-monfort-landolt.github.io/Landoltdiego',
-  plugins: [react()]
+  plugins: [react()],
+    // ...existing configuration...
+    server: {
+      historyApiFallback: true,
+    },
 })
