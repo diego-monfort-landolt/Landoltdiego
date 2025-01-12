2fe4f109a6c520d858382de69415d7332a1cdb2c
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-01-12 22:18:34 +0100

    Füge Popup für Formularstatus hinzu und aktualisiere das Formular-Design: verbesserungen folgen

--- src/component/form/Form.jsx
+++ src/component/form/Form.jsx
@@ -1,53 +1,91 @@
-import { useForm } from "react-hook-form"
-import './form.css'
+import { useForm } from "react-hook-form";
+import { useState } from 'react';
+import './form.css';
+
 const Form = () => {
-  const { register, formState: { errors } } = useForm();
+  const { register, formState: { errors }, handleSubmit } = useForm();
+  const [formStatus, setFormStatus] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await fetch('https://formsubmit.co/3524c0a9bb0dcff5dc15f2e7e57dedb3', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
+      if (response.ok) {
+        setFormStatus('success');
+        setIsSubmitted(true);
+      } else {
+        setFormStatus('error');
+      }
+    } catch (error) {
+      setFormStatus('error');
+    }
+  };
+
+  const handleOkClick = () => {
+    window.location.reload();
+  };
+
   return (
     <>
       <div className="formcontainer">
-        <form action="https://formsubmit.co/3524c0a9bb0dcff5dc15f2e7e57dedb3" method="POST" >
-          <div>
-            <label>Nombre:</label>
-            <input type="text" name="name" placeholder="Nombre" required {...register('nombre', {
-              required: true,
-              maxLength: 25
-            })} />
-            {errors.nombre?.type === 'required' && <p className="errorpara">Dime tu nombre</p>}
-            {errors.nombre?.type === 'maxLength' && <p className="errorpara">Tu nombre no puede superar los 25 caracteres</p>}
+        {!isSubmitted ? (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <label>Nombre:</label>
+              <input type="text" name="name" placeholder="Nombre" required {...register('nombre', {
+                required: true,
+                maxLength: 25
+              })} />
+              {errors.nombre?.type === 'required' && <p className="errorpara">Dime tu nombre</p>}
+              {errors.nombre?.type === 'maxLength' && <p className="errorpara">Tu nombre no puede superar los 25 caracteres</p>}
+            </div>
+            <div>
+              <label>E-Mail:</label>
+              <input type="email" name="email" placeholder="E-Mail" required {...register('email')} />
+              {errors.email?.type === 'pattern' && <p className="errorpara">El formato del e-mail está incorrecto</p>}
+            </div>
+            <div>
+              <label>Empresa:</label>
+              <input type="text" name="company" placeholder="company" {...register('empresa', {
+                required: true
+              })} />
+            </div>
+            <div>
+              <label>Descripción:</label>
+              <textarea type="text" name="message" placeholder="Tu texto..." required {...register('description', {
+                required: true
+              })} />
+              {errors.description?.type === 'required' && <p className="errorpara">¿No quieres dejarme una nota?</p>}
+            </div>
+            <div>
+              <label>Pais</label>
+              <select {...register('pais')}>
+                <option value='es'>Español</option>
+                <option value='de'>Deutsch</option>
+                <option value='en'>English</option>
+              </select>
+            </div>
+            <input type="submit" value='Enviar' className="bn632-hover bn19" />
+            <input type="hidden" name="_next" value='https://diego-monfort-landolt.github.io/Landoltdiego/'></input>
+            <input type="hidden" name="_captcha" value="true" />
+            <input type="hidden" name="_blacklist" value="vendo, telefono, link, amor, curso, formacion, dinero, http, https"></input>
+          </form>
+        ) : (
+          <div className={`popup ${formStatus === 'success' ? 'success' : 'error'}`}>
+            {formStatus === 'success' ? 'Das Formular wurde erfolgreich gesendet!' : 'Es gab ein Problem beim Senden des Formulars. Bitte versuche es erneut.'}
+            {formStatus === 'success' && <button onClick={handleOkClick} className="ok-button">Ok</button>}
           </div>
-          <div>
-            <label>E-Mail:</label>
-            <input type="email" name="email" placeholder="E-Mail" required {...register('email')} />
-            {errors.email?.type === 'pattern' && <p className="errorpara">El formato del e-mail está incorrecto</p>}
-          </div>
-          <div>
-            <label>Empresa:</label>
-            <input type="text" name="company" placeholder="company" {...register('empresa', {
-              required: true
-            })} />
-          </div>
-          <div>
-            <label>Descripción:</label>
-            <textarea type="text" name="message" placeholder="Tu texto..." required {...register('description', {
-              required: true
-            })} />
-            {errors.description?.type === 'required' && <p className="errorpara">¿No quieres dejarme una nota?</p>}
-          </div>
-          <div>
-            <label>Pais</label>
-            <select {...register('pais')}>
-              <option value='es'>Español</option>
-              <option value='de'>Deutsch</option>
-              <option value='en'>English</option>
-            </select>
-          </div>
-          <input type="submit" value='Enviar' className="bn632-hover bn19" />
-          <input type="hidden" name="_next" value='https://diego-monfort-landolt.github.io/Landoltdiego/'></input>
-          <input type="hidden" name="_captcha" value="true" />
-          <input type="hidden" name="_blacklist" value="vendo, telefono, link, amor, curso, formacion, dinero, http, https"></input>
-        </form>
+        )}
       </div>
     </>
-  )
-}
-export default Form
+  );
+};
+
+export default Form;
